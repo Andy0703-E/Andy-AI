67bfead09a960a1b6e1e476d1e943a13a196e676
Andi Agung <dadung2707@gmail.com>
2024-12-18 14:00:22 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -65,7 +65,7 @@ function App() {
 
   return (
     <main className="flex flex-col min-h-screen justify-between items-center bg-primary-color rounded-lg p-4">
-      <header className="w-full bg-gradient-to-r py-6 rounded-lg flex items-center px-6 justify-center md:justify-start">
+      <header className="w-full bg-gradient-to-r py-6 rounded-lg flex items-center px-6 justify-center md:justify-center">
         <h1 className="text-4xl text-center text-white font-semibold tracking-wider text-shadow animate-pulse">
           <span className="text-blue-400">ANDY</span> <span className="text-pink-400">AI</span>
         </h1>
